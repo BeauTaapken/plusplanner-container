4d446db5fc2b0cf09791e5d273e4a043fe661d9c
Niek125 <niek.sleddens@gmail.com>
2019-12-16 08:56:12 +0100

    change model

--- src/main/java/plus/planner/containerservice/model/Part.java
+++ src/main/java/plus/planner/containerservice/model/Part.java
@@ -1,6 +1,5 @@
 package plus.planner.containerservice.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,9 +7,6 @@ import lombok.Setter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
 
 @Entity
 @NoArgsConstructor
@@ -21,14 +17,9 @@ import javax.validation.constraints.Null;
 @EntityListeners(AuditingEntityListener.class)
 public class Part {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long partid;
-    @Null
-    @JsonIgnore
     private Long componentid;
-    @NotBlank
     private String partname;
-    @NotBlank
     private String enddate;
     @Transient
     private String subparts;
